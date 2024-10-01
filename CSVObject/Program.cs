695d7f5c6f17f993b89ef5babf056ba312360939
ferdavs <ferdavs@naver.com>
2024-10-01 23:28:05 +0900

    Add project files.

---
+++ CSVObject/Program.cs
@@ -0,0 +1,7 @@
+﻿using CSVObject.File;
+
+CsvConverter csvConverter = new CsvConverter();
+
+List<DynamicRow> list = csvConverter.ReadCsvFile(@"C:\test.csv");
+string filePaths = csvConverter.GetListOfCsvFiles(@"C:\");
+csvConverter.MoveFileAccordingly(@"C:\test.csv",false);
